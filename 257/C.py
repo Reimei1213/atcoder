d5372182702ce970763308f94d902e87ab3294d9
reimei <34594195+Reimei1213@users.noreply.github.com>
2022-08-18 01:44:31 +0900

    fix

--- 257/C.py
+++ 257/C.py
@@ -2,37 +2,20 @@ N = int(input())
 S = input()
 W = list(map(int, input().split()))
 
-adult = []
-child = []
+people = []
+count = 0
 for n in range(N):
+    people.append((W[n], S[n]))
     if S[n] == '1':
-        adult.append(W[n])
-    else:
-        child.append(W[n])
-adult.sort()
-child.sort()
-adult_num = len(adult)
-child_num = len(child)
+        count += 1
 
-i = 0
-j = 0
-ans = 0
-tmp = 0
-while True:
-    tmp = adult_num - i + j
-    ans = max(tmp, ans)
-    if i == adult_num and j == child_num:
-        break
-    elif i == adult_num:
-        j += 1
-    elif j == child_num:
-        i += 1
-    else:
-        if adult[i] < child[j]:
-            i += 1
-        elif adult[i] > child[j]:
-            j += 1
-        else:
-            i += 1
-            j += 1
-print(ans)
+people.sort()
+result = count
+for n in range(N):
+    w, s = people[n]
+    if s == '1': count -= 1
+    if s == '0': count += 1
+    if n+1 == N or people[n+1][0] != w:
+        result = max(result, count)
+
+print(result)
